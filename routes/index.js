4516987003a7013eeb8178966a73ab8c35f39201
Matthias De Fré <matthiasdefre@Matthiass-MacBook-Air.local>
2019-01-20 18:43:23 +0100

    Changed event type of BGame

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 var maps = []
 maps["AGame"] =
-  { "completedEvents": [], "map": [["Water", "Water", "Water"], ["Lumberyard", "Lumberyard", "Lumberyard"], ["Land", "Land", "Land"]], "name": "ThirdMap", "undiscoveredEvents": { "2": { "values": ["W", "500"], "id": 2, "successorsIds": [], "title": "Test sedcond event", "type": "WinR", "description": "sfddsfsddfsfd", "actions": [] } }, "possibleEvents": [{ "values": ["W", "500"], "id": 1, "successorsIds": [2, 3], "title": "Wood Festival", "type": "WinR", "description": "Due to wood festival you will need to trade either gold or stone with other cities. Gain 500 but lose 500 of the chosen material", "actions": [{ "value": ["G", "1000"], "type": "LoseR", "description": "Lose Gold" }, { "value": ["S", "1000"], "type": "LoseR", "description": "Lose Stone" }] }] }
+  { "completedEvents": [], "map": [["Water", "Water", "Water"], ["Lumberyard", "Lumberyard", "Lumberyard"], ["Land", "Land", "Land"]], "name": "AGame", "undiscoveredEvents": { "2": { "values": ["W", "500"], "id": 2, "successorsIds": [], "title": "Test sedcond event", "type": "WinR", "description": "sfddsfsddfsfd", "actions": [] } }, "possibleEvents": [{ "values": ["W", "500"], "id": 1, "successorsIds": [2, 3], "title": "Wood Festival", "type": "WinR", "description": "Due to wood festival you will need to trade either gold or stone with other cities. Gain 500 but lose 500 of the chosen material", "actions": [{ "value": ["G", "1000"], "type": "LoseR", "description": "Lose Gold" }, { "value": ["S", "1000"], "type": "LoseR", "description": "Lose Stone" }] }] }
 maps.keys
 
 /* GET home page. */
